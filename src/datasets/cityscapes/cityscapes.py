d81fdccd3c8b1468bc1eac35103ccf72db0ad638
Ishfaq Ahmad Bhat <96306723+Ishfaz@users.noreply.github.com>
2024-11-21 23:35:04 +0100

    Update cityscapes.py

--- src/datasets/cityscapes/cityscapes.py
+++ src/datasets/cityscapes/cityscapes.py
@@ -1,42 +1,62 @@
-########################################################
-#                                                      #
-#       author: omitted for anonymous submission       #
-#                                                      #
-#     credits and copyright coming upon publication    #
-#                                                      #
-########################################################
-
-
-from cityscapesscripts.helpers.labels import labels
-
 
 class CityscapesBase:
+    # Define splits available in your dataset
     SPLITS = ["train", "valid", "test", "val"]
 
-    # number of classes without void/unlabeled and license plate (class 34)
-    N_CLASSES = [19, 33]
+    # Define number of classes (10 classes + void)
+    N_CLASSES = [11]
 
-    # 1+33 classes (0: unlabeled)
-    CLASS_NAMES_FULL = [label.name for label in labels]
-    CLASS_COLORS_FULL = [label.color for label in labels]
+    # Class names and colors for your dataset
+    CLASS_NAMES_FULL = [
+        'Void',            # 0
+        'Ship hull',       # 1
+        'Marine growth',   # 2
+        'Anode',          # 3
+        'Overboard valve', # 4
+        'Propeller',      # 5
+        'Paint peel',     # 6
+        'Bilge keel',     # 7
+        'Defect',         # 8
+        'Corrosion',      # 9
+        'Sea chest grating'# 10
+    ]
 
-    # 1+19 classes (0: void)
-    CLASS_NAMES_REDUCED = ["void"] + [
-        label.name for label in labels if not label.ignoreInEval
+    CLASS_COLORS_FULL = [
+        (0, 0, 0),         # Void - Black
+        (0, 0, 255),       # Ship hull - Blue
+        (0, 128, 0),       # Marine growth - Green
+        (0, 255, 255),     # Anode - Cyan
+        (64, 224, 208),    # Overboard valve - Turquoise
+        (128, 0, 128),     # Propeller - Purple
+        (255, 0, 0),       # Paint peel - Red
+        (255, 165, 0),     # Bilge keel - Orange
+        (255, 192, 203),   # Defect - Pink
+        (255, 255, 0),     # Corrosion - Yellow
+        (255, 255, 255),   # Sea chest grating - White
     ]
-    CLASS_COLORS_REDUCED = [(0, 0, 0)] + [
-        label.color for label in labels if not label.ignoreInEval
-    ]
-    # forward mapping (0: unlabeled) + 33 classes -> (0: void) + 19 classes
+
+    # Since we're not reducing classes, use the same for reduced
+    CLASS_NAMES_REDUCED = CLASS_NAMES_FULL
+    CLASS_COLORS_REDUCED = CLASS_COLORS_FULL
+
+    # Mapping classes (we're not reducing, so straight mapping)
     CLASS_MAPPING_REDUCED = {
-        c: labels[c].trainId + 1 if not labels[c].ignoreInEval else 0
-        for c in range(1 + 33)
+        0: 0,     # Void
+        1: 1,     # Ship hull
+        2: 2,     # Marine growth
+        3: 3,     # Anode
+        4: 4,     # Overboard valve
+        5: 5,     # Propeller
+        6: 6,     # Paint peel
+        7: 7,     # Bilge keel
+        8: 8,     # Defect
+        9: 9,     # Corrosion
+        10: 10    # Sea chest grating
     }
 
-    RGB_DIR = "rgb"
-
-    LABELS_FULL_DIR = "labels_33"
-    LABELS_FULL_COLORED_DIR = "labels_33_colored"
-
-    LABELS_REDUCED_DIR = "labels_19"
-    LABELS_REDUCED_COLORED_DIR = "labels_19_colored"
+    # Directory structure (matching your processed dataset)
+    RGB_DIR = "leftImg8bit"  # Directory containing your RGB images
+    LABELS_FULL_DIR = "gtFine"  # Directory containing your ground truth labels
+    LABELS_FULL_COLORED_DIR = "gtFine_color"  # Colored visualizations
+    LABELS_REDUCED_DIR = "gtFine"  # Same as full since we don't reduce classes
+    LABELS_REDUCED_COLORED_DIR = "gtFine_color"  # Same as full colored
